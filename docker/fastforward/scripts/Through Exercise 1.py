e2145cf0df76cfb3f032adf943a1f14d6cfcbd61
Dax Mickelson <dmickels@cisco.com>
2020-02-04 14:34:22 -0700

    troubleshooting

--- docker/fastforward/scripts/Through Exercise 1.py
+++ docker/fastforward/scripts/Through Exercise 1.py
@@ -67,7 +67,7 @@ def initial_discover(api, data_vars):
     while len(devices_discovered) < len(discovery_info["device_names"]):
         result = api.devices.get_device_list()
         for device in result["response"]:
-            print(result["response"])
+            print(f"List of devices to find: {discovery_info['device_names']}\nDevices found: {devices_discovered}")
             if device["hostname"] in discovery_info["device_names"]:
                 print(f"{device['hostname']} has been added to inventory.")
                 discovery_info["device_names"].remove(device["hostname"])
