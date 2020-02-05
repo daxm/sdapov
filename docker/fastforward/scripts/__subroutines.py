a62b0cd52ec57ba2a5ea08bc97ad9a825c22808a
Dax Mickelson <dmickels@cisco.com>
2020-02-05 16:48:27 -0700

    troubleshooting assign_device_to_sites()

--- docker/fastforward/scripts/__subroutines.py
+++ docker/fastforward/scripts/__subroutines.py
@@ -25,9 +25,10 @@ def assign_devices_to_sites(api_connection, data_vars, devices=[]):
             if yaml_device["name"] == device:
                 # Get ID of hierarchy location for this device.
                 print(f"Assigning {device} to site {yaml_device['location_name']}")
+                print(api_connection.sites.get_site(name=yaml_device["location_name"]))
                 api_connection.sites.assign_device_to_site(
                     device=the_device,
-                    site_id=api_connection.sites.get_site(name=yaml_device["location_name"])["response"][0]["id"])
+                    site_id='')
 
 
 def get_device_by_name(api_connection, name=None):
